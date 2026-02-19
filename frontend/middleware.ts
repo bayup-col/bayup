61bed071733d62c08fda644b57870dc5bf71af8c
Daniel-quin <danquin9713@gmail.com>
2026-02-19 08:51:36 -0500

    vercel

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -15,7 +15,7 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth().protect();
+    await auth.protect();
   }
 });
 
